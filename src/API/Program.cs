ada947225014fbdc98d072a4a17af8715a0b9d23
Adam Eleraky <adam_eleraky@hotmail.com>
2025-11-28 09:17:42 +0100

    Added vercel url to connect with render backend

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -31,7 +31,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173") // React dev server
+            policy.WithOrigins("http://localhost:5173", "https://landit-frontend.vercel.app")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
